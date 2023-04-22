d0c0df96156fcd57638d13e2d2b7f47a8b825d3d
Alaqian Zafar <a7zafar@gmail.com>
2023-04-22 02:08:51 -0400

    Add UNET

--- CA04/imports.py
+++ CA04/imports.py
@@ -5,5 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
+from torchvision import transforms
